267e2e6ea2c474cb3e792c3daadfd3878a5528ea
ginn1111 <vanthuanjw@gmail.com>
2023-10-19 10:23:05 +0700

    chore: typo name of database

--- src/controllers/nhan-vien.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from "express";
-
-export const getList = (_: any, res: any) => {
-	res.status(200).json("abcd");
-};
-export const getDetail = () => {};
-export const addNew = () => {};
-export const update = () => {};
-export const remove = () => {};
