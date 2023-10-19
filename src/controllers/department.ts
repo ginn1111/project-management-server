daa3c607b80ba45b04aaec0e8afad10c885148b4
ginn1111 <vanthuanjw@gmail.com>
2023-10-19 23:34:53 +0700

    feat: add new endpoint for department

---
+++ src/controllers/department.ts
@@ -0,0 +1,257 @@
+import { PrismaClient } from "@prisma/client";
+import { Response } from "express";
+import { isEmpty } from "lodash";
+import { IDepartmentRequest } from "../@types/request";
+import { generateId } from "../utils/generate-id";
+
+const PREFIX_KEY = "DEPA";
+const prismaClient = new PrismaClient();
+
+export const getListByEmployee = async (
+	req: IDepartmentRequest,
+	res: Response,
+) => {
+	const { id } = req.params;
+
+	try {
+		if (!id) return res.status(422).json("invalid parameters");
+		const departmentOfEmployee =
+			await prismaClient.employeesOfDepartment.findMany({
+				where: {
+					idEmployee: id,
+				},
+				include: {
+					department: true,
+				},
+			});
+		return res.json(departmentOfEmployee);
+	} catch (error) {
+		console.log(error);
+		return res.status(500).json(error);
+	}
+};
+
+export const getList = async (req: IDepartmentRequest, res: Response) => {
+	let { page, limit, search = "" } = req.query ?? {};
+	const _page = !isNaN(page as unknown as number) ? parseInt(page!) : 0;
+	const _limit = !isNaN(limit as any) ? parseInt(limit!) : 10;
+
+	try {
+		const totalItems = await prismaClient.department.count({
+			where: {
+				AND: [
+					{
+						isActive: true,
+					},
+				],
+				OR: [
+					{
+						name: {
+							endsWith: search,
+						},
+					},
+
+					{
+						name: {
+							startsWith: search,
+						},
+					},
+				],
+			},
+		});
+
+		const departments = await prismaClient.department.findMany({
+			take: _limit,
+			skip: _page * _limit,
+			where: {
+				AND: [
+					{
+						isActive: true,
+					},
+				],
+				OR: [
+					{
+						name: {
+							endsWith: search,
+						},
+					},
+
+					{
+						name: {
+							startsWith: search,
+						},
+					},
+				],
+			},
+		});
+
+		return res.status(200).json({ departments, totalItems });
+	} catch (error) {
+		console.log(error);
+		return res.status(500).json(error);
+	}
+};
+
+export const update = async (req: IDepartmentRequest, res: Response) => {
+	const { id } = req.params;
+	try {
+		if (!id || isEmpty(req.body)) {
+			return res.status(422).json("invalid parameters");
+		}
+		const department = await prismaClient.department.findUnique({
+			where: {
+				id,
+			},
+		});
+
+		if (isEmpty(department)) return res.status(422).json("invalid parameters");
+
+		const { id: _, ...rest } = department;
+		const _updatedDepartment = Object.assign(rest, req.body);
+
+		const updatedDepartment = await prismaClient.department.update({
+			data: {
+				..._updatedDepartment,
+			},
+			where: {
+				id,
+			},
+		});
+
+		return res.json(updatedDepartment);
+	} catch (error) {
+		console.log(error);
+		return res.status(500).json(error);
+	}
+};
+
+export const updateByEmployee = async (
+	req: IDepartmentRequest,
+	res: Response,
+) => {
+	const { idRelation } = req.params;
+	const { startDate, endDate, ...restBody } = req.body ?? {};
+
+	try {
+		if (!idRelation) return res.status(422).json("invalid parameters");
+
+		const departmentOfEmployee =
+			await prismaClient.employeesOfDepartment.findFirst({
+				where: {
+					id: idRelation,
+				},
+			});
+
+		if (isEmpty(departmentOfEmployee))
+			return res.status(422).json("invalid parameters");
+
+		const { id, idEmployee, idDepartment, ...rest } = departmentOfEmployee;
+		const _updatedDepartmentOfEmployee = Object.assign(rest, {
+			startDate: startDate ? new Date(startDate).toISOString() : rest.startDate,
+			endDate: endDate ? new Date(endDate).toISOString() : rest.endDate,
+			...restBody,
+		});
+
+		const updatedDepartmentByEmployee =
+			await prismaClient.employeesOfDepartment.update({
+				data: _updatedDepartmentOfEmployee,
+				where: {
+					id: idRelation,
+				},
+			});
+		return res.json(updatedDepartmentByEmployee);
+	} catch (error) {
+		console.log(error);
+		return res.status(500).json(error);
+	}
+};
+
+export const addNew = async (req: IDepartmentRequest, res: Response) => {
+	try {
+		if (isEmpty(req.body)) {
+			return res.status(422).json("invalid parameters");
+		}
+		const { id, ...restBody } = req.body;
+		const department = await prismaClient.department.create({
+			data: {
+				id: generateId(PREFIX_KEY),
+				...restBody,
+			},
+		});
+		return res.status(200).json(department);
+	} catch (error) {
+		console.log(error);
+		return res.status(500).json(error);
+	}
+};
+
+export const addToEmployee = async (
+	req: IDepartmentRequest<{ idEmp: string }>,
+	res: Response,
+) => {
+	const { id, idEmp } = req.params;
+	const { startDate, endDate, ...restBody } = req.body ?? {};
+
+	try {
+		if (!id || !idEmp) return res.status(422).json("invalid parameters");
+
+		const createdDepartmentOfEmployee =
+			await prismaClient.employeesOfDepartment.create({
+				data: {
+					id: generateId("EMDE"),
+					idDepartment: id,
+					idEmployee: idEmp,
+					...restBody,
+					startDate: startDate ? new Date(startDate).toISOString() : null,
+					endDate: endDate ? new Date(endDate).toISOString() : null,
+				},
+			});
+
+		return res.json(createdDepartmentOfEmployee);
+	} catch (error) {
+		console.log(error);
+		return res.status(500).json(error);
+	}
+};
+
+export const getDetail = async (req: IDepartmentRequest, res: Response) => {
+	const { id } = req.params;
+	try {
+		if (!id) {
+			return res.status(422).json("invalid parameters");
+		}
+		const department = await prismaClient.department.findFirst({
+			where: {
+				id,
+			},
+			include: {
+				employeesOfDepartment: true,
+			},
+		});
+
+		return res.status(200).json(department);
+	} catch (error) {
+		console.log(error);
+		return res.status(500).json(error);
+	}
+};
+
+export const remove = async (req: IDepartmentRequest, res: Response) => {
+	const { id } = req.params;
+	try {
+		if (!id) return res.status(422).json("invalid parameters");
+
+		const deletedDepartment = await prismaClient.department.update({
+			data: {
+				isActive: false,
+			},
+			where: {
+				id,
+			},
+		});
+		return res.json(deletedDepartment);
+	} catch (error) {
+		console.log(error);
+		return res.status(500).json(error);
+	}
+};
--- src/routes/department.ts
+++ src/routes/department.ts
@@ -1,5 +1,46 @@
-import { Router } from "express";
+import { faker } from "@faker-js/faker";
+import { PrismaClient } from "@prisma/client";
+import { Response, Router } from "express";
+import {
+	addNew,
+	getDetail,
+	update,
+	getListByEmployee,
+	getList,
+	updateByEmployee,
+	addToEmployee,
+	remove,
+} from "../controllers/department";
+import { generateId } from "../utils/generate-id";
 
+const prismaClient = new PrismaClient();
 const departmentRouter = Router();
+
+departmentRouter.get("/", getList);
+departmentRouter.get("/:id/employee", getListByEmployee);
+departmentRouter.get("/:id", getDetail);
+departmentRouter.post("/add", addNew);
+departmentRouter.post("/add/:id/:idEmp/employee", addToEmployee);
+departmentRouter.patch("/:idRelation/update/employee", updateByEmployee);
+departmentRouter.patch("/:id/update", update);
+departmentRouter.delete("/:id/remove", remove);
+
+departmentRouter.post("/random-depa", async (_, res: Response) => {
+	const createdDepartment = await prismaClient.department.create({
+		data: {
+			id: generateId("DEPA"),
+			name: faker.internet.displayName(),
+			note: faker.string.sample(),
+			phone: faker.phone.number(),
+			employeesOfDepartment: {
+				create: {
+					id: generateId("EMDE"),
+					idEmployee: "EMPL_01HCYJHDATA3XECHSYBBPNF28K",
+				},
+			},
+		},
+	});
+	return res.json(createdDepartment);
+});
 
 export default departmentRouter;
